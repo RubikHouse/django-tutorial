d49c0c9858e8f108972f202f8c8bf446e0d8efbb
RubikHouse <ni37fsl@gmail.com>
2018-10-02 16:00:40 +0900

    index, detail, results, vote view를 url과 연결, index 뷰는 가장 최근에 발행된 Question을 최대 5개까지 보여줌

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -15,4 +17,7 @@ def vote(request, question_id):
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    print(latest_question_list)
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
