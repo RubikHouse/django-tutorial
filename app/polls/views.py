51506097da438664577d45fe2cb6daf5d9d72533
RubikHouse <ni37fsl@gmail.com>
2018-10-02 16:42:59 +0900

    detail.html 관련 내용 추가

--- app/polls/views.py
+++ app/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.template import loader
 
@@ -24,3 +24,11 @@ def index(request):
         'latest_question_list': latest_question_list,
     }
     return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
