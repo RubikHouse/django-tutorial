75ccfc44976b1bdf6446086cb7691a7158b9f641
RubikHouse <ni37fsl@gmail.com>
2018-10-04 16:07:24 +0900

    튜토리얼 끝

--- app/polls/tests.py
+++ app/polls/tests.py
@@ -14,7 +14,7 @@ def create_question(question_text, days):
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
-        response = self.client.get(reverse('polls:Index'))
+        response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No polls are available")
         self.assertQuerysetEqual(response.context['latest_question_list'], [])
@@ -26,15 +26,15 @@ class QuestionIndexViewTests(TestCase):
                                  ['<Question: Past question.>'])
 
     def test_future_question(self):
-        create_question(question_text="Future question", days=30)
+        create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context['latest_question_list'],
                                  [])
 
     def test_future_question_and_past_question(self):
-        create_question(question_text="Past question", days=-30)
-        create_question(question_text="Future question", days=30)
+        create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
 
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(response.context['latest_question_list'],
@@ -47,12 +47,12 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(response.context['latest_question_list'],
                                  ['<Question: Past question 2.>',
-                                  'Question: Past question 1.>'])
+                                  '<Question: Past question 1.>'])
 
 
-class QuestionModelTests(TestCase):
-
-    def test_was_published_recently_with_future_question(self):
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
-        self.assertIs(future_question.was_published_recently(), False)
+# class QuestionModelTests(TestCase):
+#
+#     def test_was_published_recently_with_future_question(self):
+#         time = timezone.now() + datetime.timedelta(days=30)
+#         future_question = Question(pub_date=time)
+#         self.assertIs(future_question.was_published_recently(), False)
